479db00de3a4156b3154fdfaef6aab209eeb83d7
ByeongHyeon <qudgus1984@naver.com>
2022-04-12 21:55:10 +0900

    새 목록 추가

--- 11.Table/AddViewController.swift
+++ 11.Table/AddViewController.swift
@@ -17,6 +17,10 @@ class AddViewController: UIViewController {
     }
     
     @IBAction func btnAddItem(_ sender: UIButton) {
+        items.append(tfAddItem.text!) // items에 텍스트 필드의 텍스트 값을 추가
+        itemsImageFile.append("clock.png") // itemsImageFile에는 무조건 clock.png파일을 추가
+        tfAddItem.text="" // 텍스트 필드의 내용을 지움
+        _ = navigationController?.popViewController(animated: true) // 루트 뷰 컨트롤러, 즉 테이블 뷰로 돌아감
     }
     
     /*
