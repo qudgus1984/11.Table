6b6510cdcfec29656af8a32908d5aadf50e0c607
ByeongHyeon <qudgus1984@naver.com>
2022-04-12 22:02:22 +0900

    목록 아이템 추가

--- 11.Table/DetailViewController.swift
+++ 11.Table/DetailViewController.swift
@@ -8,14 +8,22 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    var receiveItem = "" // Main View에서 받을 텍스트를 위해 변수 receiveItem을 선언함
 
     @IBOutlet var lblItem: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        lblItem.text = receiveItem // 뷰가 노출될 때마다 이 내용을 레이블 텍스트로 표시
     }
     
+    
+    func receiveItem(_ item: String) // Main View에서 변수를 받기 위한 함수를 추가함
+    {
+        receiveItem = item
+    }
 
     /*
     // MARK: - Navigation
